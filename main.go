a09017ce629985d7930d99abc77aa44ef07d8cf3
Anamel Yilma <anamelyilma0@gmail.com>
2026-05-30 15:44:59 +0300

    main go

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
-
 package main
 
 import (
 	"fmt"
 	"log"
-
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/recover"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var DB *gorm.DB
 
 func main() {
 	// Database connection
-	dsn := "host=localhost user=postgres password=password dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	
+	dsn := "host=localhost user=postgres password=0909 dbname=Rag-System port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("err happen : database is not connect ", err)
+	}
+	DB = db
+
+	app := fiber.New()
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	fmt.Print("Hellow world My program is running")
 
 }
