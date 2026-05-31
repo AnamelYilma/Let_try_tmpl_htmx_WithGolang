979e57549e8079f7e55e12fee4cfcddf10c44c21
Anamel Yilma <anamelyilma0@gmail.com>
2026-05-31 22:21:33 +0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -2,12 +2,14 @@ package database
 
 import (
 	"fmt"
+	"gootmplhtmx/Route"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var task Model.TASK
 
 func DbLoad() (*gorm.DB, error) {
 	dsn := "host=localhost user=postgres password=0909 dbname=Rag-System port=5432 sslmode=disable"
@@ -15,6 +17,9 @@ func DbLoad() (*gorm.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
+	
 	DB = db
+	db.AutoMigrate(&task)
+
 	return db, nil
 }
