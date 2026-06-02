3b411b1aece1a5e8f95277fb0025ab790e64c3d3
Anamel Yilma <anamelyilma0@gmail.com>
2026-06-02 12:43:00 +0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 var task model.TASK
 
 func DbLoad() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=0909 dbname=Rag-System port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=0909 dbname=serverside port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
