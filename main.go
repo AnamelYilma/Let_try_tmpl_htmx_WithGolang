8d158cf8fb5c9f51ec2148cb42d840e0ef66c49b
Anamel Yilma <anamelyilma0@gmail.com>
2026-06-06 16:47:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,25 @@
 package main
-
 import (
 	"log"
-
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/gofiber/fiber/v3/middleware/static"
-
 	"gootmplhtmx/Route"
 	"gootmplhtmx/database"
 )
-
 func main() {
 	if _, err := database.DbLoad(); err != nil {
 		log.Fatal(err)
 	}
-
-	// Create Fiber app with v3
 	APP := fiber.New()
-
-	// Add middleware
 	APP.Use(logger.New())
 	APP.Use(recover.New())
 	APP.Use("/static", static.New("./public"))
 	Route.Routing(APP)
-
-	log.Fatal(APP.Listen(":3000"))
-
+	// Print all registered routes
+	for _, route := range APP.GetRoutes(true) {
+		log.Printf("Route: %s %s", route.Method, route.Path)
+	}
+	log.Fatal(APP.Listen(":4000"))
 }
