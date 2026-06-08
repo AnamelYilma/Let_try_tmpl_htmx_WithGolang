82850e2320a66acca8079330630a508cc5bdc312
Anamel Yilma <anamelyilma0@gmail.com>
2026-06-08 13:25:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
+
 import (
 	"log"
+	"os"
+
+	"gootmplhtmx/Route"
+	"gootmplhtmx/database"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/gofiber/fiber/v3/middleware/static"
-	"gootmplhtmx/Route"
-	"gootmplhtmx/database"
 )
+
 func main() {
 	if _, err := database.DbLoad(); err != nil {
 		log.Fatal(err)
 	}
-	APP := fiber.New()
-	APP.Use(logger.New())
-	APP.Use(recover.New())
-	APP.Use("/static", static.New("./public"))
-	Route.Routing(APP)
-	// Print all registered routes
-	for _, route := range APP.GetRoutes(true) {
-		log.Printf("Route: %s %s", route.Method, route.Path)
+
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use("/static", static.New("./public"))
+	Route.Routing(app)
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "4000"
 	}
-	log.Fatal(APP.Listen(":4000"))
+
+	log.Fatal(app.Listen(":" + port))
 }
